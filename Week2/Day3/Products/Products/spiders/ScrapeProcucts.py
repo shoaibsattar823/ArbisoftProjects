9f9dad30ac09b032d0f70dfcae1e1069fe051414
Shoaib Sattar <shoaibsattar823@gmail.com>
2017-07-19 17:32:24 +0500

    Simple Scrape

---
+++ Week2/Day3/Products/Products/spiders/ScrapeProcucts.py
@@ -0,0 +1,32 @@
+import scrapy
+
+
+class ProductSpider(scrapy.Spider):
+    name = "products"
+    nextp = 0
+    # brand = input('Enter the brand name you want to scrape: ')
+
+    start_urls = [
+              'https://www.daraz.pk/catalog/?q='
+              '{brand}'.format(brand='samsung'),
+    ]
+
+    def parse(self, response):
+        for prod in response.css('section.products div.sku'):
+            yield {
+                'image': prod.css('div.image-wrapper img'
+                                  '::attr(data-src)').extract_first(),
+                'brand': prod.css('h2.title span.brand::text').extract_first(),
+                'title': prod.css('h2.title span.name::text').extract_first(),
+                'price(PKR)': prod.css('span.price span'
+                                       '::attr(data-price)').extract_first(),
+            }
+        nextpage = response.css('section.pagination [title=Next]'
+                                '::attr(href)').extract_first()
+
+        self.nextp = self.nextp + 1
+
+        if (self.nextp != 3):
+            yield response.follow(nextpage, callback=self.parse)
+            # nextpage = response.urljoin(nextpage)
+            # yield scrapy.Request(nextpage, callback=self.parse)
