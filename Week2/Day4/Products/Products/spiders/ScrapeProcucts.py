a7cc032f3a99277af39b4051c48ae8007b3a6145
Shoaib Sattar <shoaibsattar823@gmail.com>
2017-07-20 17:12:55 +0500

    Scraping details - code modified

--- Week2/Day4/Products/Products/spiders/ScrapeProcucts.py
+++ Week2/Day4/Products/Products/spiders/ScrapeProcucts.py
@@ -1,6 +1,16 @@
 import scrapy
 
 
+class ProductItem(scrapy.Item):
+    image = scrapy.Field()
+    brand = scrapy.Field()
+    title = scrapy.Field()
+    price_PKR = scrapy.Field()
+    # detailpage = scrapy.Field()
+    det_title = scrapy.Field()
+    det_features = scrapy.Field()
+
+
 class ProductSpider(scrapy.Spider):
     name = "products"
     nextp = 0
@@ -15,27 +25,30 @@ class ProductSpider(scrapy.Spider):
     def parse(self, response):
         for prod in response.css('section.products div.sku'):
             detailpage = prod.xpath('./a/@href').extract_first()
-            yield scrapy.Request(detailpage, self.parse_detail, meta={'item': {
-                'image': prod.css('div.image-wrapper img'
-                                  '::attr(data-src)').extract_first(),
-                'brand': prod.css('h2.title span.brand::text').extract_first(),
-                'title': prod.css('h2.title span.name::text').extract_first(),
-                'price(PKR)': prod.css('span.price span'
-                                       '::attr(data-price)').extract_first(),
-            }})
+            image = prod.css('div.image-wrapper img'
+                             '::attr(data-src)').extract_first()
+            brand = prod.css('h2.title span.brand::text').extract_first()
+            title = prod.css('h2.title span.name::text').extract_first()
+            price_PKR = prod.css('span.price span'
+                                 '::attr(data-price)').extract_first()
+            item = ProductItem()
+            item['image'] = image
+            item['brand'] = brand
+            item['title'] = title
+            item['price_PKR'] = price_PKR
+            if detailpage:
+                yield scrapy.Request(detailpage,
+                                     self.parse_detail, meta={'item': item})
+            else:
+                return
 
-            # print 'detailpage = ', detailpage
-            # detail = yield response.follow(
-            #                 detailpage, callback=self.parse_detail)
-            # print('\n****detail here: %s' % detail)
+        nextpage = response.css('section.pagination [title=Next]'
+                                '::attr(href)').extract_first()
 
-        # nextpage = response.css('section.pagination [title=Next]'
-        #                        '::attr(href)').extract_first()
+        self.nextp = self.nextp + 1
 
-        # self.nextp = self.nextp + 1
-
-        # if (self.nextp != 3):
-        #     yield response.follow(nextpage, callback=self.parse)
+        if (self.nextp != 3):
+            yield response.follow(nextpage, callback=self.parse)
 
     def parse_detail(self, response):
         item = response.meta['item']
