cfa3306369c79068d7a0e5f61758c97d1fcff983
Shoaib Sattar <shoaibsattar823@gmail.com>
2017-07-25 19:30:26 +0500

    Django Intro

---
+++ Week3/Day2/mysite/mysite/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.http import Http404
+
+import datetime
+
+
+def hello(request):
+    return HttpResponse("Hello WORLD")
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html><body>It is now %s.</body></html>' % now
+    return HttpResponse(html)
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = ('<html><body>In {of} hour(s)'
+            ', it will be  {dt}.</body></html>'
+            ).format(of=offset, dt=dt)
+    return HttpResponse(html)
