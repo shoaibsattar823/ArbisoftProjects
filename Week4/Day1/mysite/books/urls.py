ef141e1cec534c8e97404b8849f04dbbd42ad912
Shoaib Sattar <shoaibsattar823@gmail.com>
2017-07-31 19:25:45 +0500

    modified (in-built fn used)

--- Week4/Day1/mysite/books/urls.py
+++ Week4/Day1/mysite/books/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
-# from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
 from books import views
 
 urlpatterns = [
-    url(r'^login-form/$', views.login_form),
+    url(r'^login-form/$', auth_views.login, name='login'),
     url(r'^login-result/$', views.login_result),
     url(r'^books/$', views.book_list),
     url(r'^login-result/detail/([0-9+])/$', views.book_detail),
     url(r'^books/detail/([0-9+])/$', views.book_detail),
-    url(r'^books/logout/$', views.logout_view),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'},
+        name='logout'),
 ]
--- Week4/Day1/mysite/mysite/settings.py
+++ Week4/Day1/mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+LOGIN_REDIRECT_URL = '/books'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
