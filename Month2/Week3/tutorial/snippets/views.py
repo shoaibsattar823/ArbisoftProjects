950c7041ecdcf0e1ee89677fd17fc6731c78ce2d
Shoaib Sattar <shoaibsattar823@gmail.com>
2017-08-24 15:09:56 +0500

    week3-django rest framework-Day1,2,3

---
+++ Month2/Week3/tutorial/snippets/views.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from rest_framework import renderers
+
+from snippets.permissions import IsOwnerOrReadOnly
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
